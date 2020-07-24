4d04ab4e5c9c93f89dc56bfec9e8e115091da332
Nullator <oDqyi1r7V3>
2020-07-24 15:11:43 +0400

    Оформил в виде класса. Сделал выбор способа сортировки, выполнил задание в полном объеме.

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -22,40 +22,61 @@
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 from pprint import pprint
 
-stat = {}
-char_list = []
-char_stat = 0
 
-with open(file='voyna-i-mir.txt', mode='r', encoding='cp1251') as file:
-    for line in file:
-        for char in line:
-            if char.isalpha():
-                if char in stat:
-                    stat[char] += 1
-                else:
-                    stat[char] = 1
-                char_stat += 1
+class StatGenerate:
 
-char_list = list(stat.items())
-# char_list.sort()
-# pprint(char_list)
-# char_list.sort(key=lambda i: i[1])                  # по частоте по возрастанию
-# pprint(char_list)
-char_list.sort(key=lambda i: i[1], reverse=True)    # по частоте по убыванию
-# pprint(char_list)
+    def __init__(self, file_name='voyna-i-mir.txt', encoding='utf8'):
+        self.stat = {}
+        self.char_list = []
+        self.char_stat = 0
+        self.file_name = file_name
+        self.encoding = encoding
+        self.generate()
+
+    def generate(self):
+        with open(file=self.file_name, mode='r', encoding=self.encoding) as file:
+            for line in file:
+                for char in line:
+                    if char.isalpha():
+                        if char in self.stat:
+                            self.stat[char] += 1
+                        else:
+                            self.stat[char] = 1
+                        self.char_stat += 1
+        self.char_list = list(self.stat.items())
+
+    def print_stat(self):
+        print('+---------+---------+')
+        print('|  буква  | частота |')
+        print('+---------+---------+')
+        for char in self.char_list:
+            print(f'|{char[0]:^9}|{char[1]:^9}|')
+        print('+---------+---------+')
+        print(f'|  итого  |{self.char_stat:^9}|')
+        print('+---------+---------+')
+
+    def print_a_z(self):
+        self.char_list.sort()
+        self.print_stat()
+
+    def print_z_a(self):
+        self.char_list.sort(reverse=True)
+        self.print_stat()
+
+    def print_9_0(self):
+        self.char_list.sort(key=lambda i: i[1], reverse=True)
+        self.print_stat()
+
+    def print_0_9(self):
+        self.char_list.sort(key=lambda i: i[1])
+        self.print_stat()
 
 
-print('+---------+---------+')
-print('|  буква  | частота |')
-print('+---------+---------+')
-for char in char_list:
-    print(f'|{char[0]:^9}|{char[1]:^9}|')
-print('+---------+---------+')
-print(f'|  итого  |{char_stat:^9}|')
-print('+---------+---------+')
-
-
-
+war_and_peace = StatGenerate(encoding='cp1251')
+war_and_peace.print_0_9()  # - по частоте по возрастанию
+war_and_peace.print_9_0()  # - по частоте по убыванию
+war_and_peace.print_a_z()  # - по алфавиту по возрастанию
+war_and_peace.print_z_a()  # - по алфавиту по убыванию
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
