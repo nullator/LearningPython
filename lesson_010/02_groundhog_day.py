6bbd433143ccbf0e98f2b2aef06141cec2e9ab19
nullator <diver.rus@gmail.com>
2020-08-08 16:04:09 +0400

    Выполнил второе учебное задание

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -16,9 +16,57 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
+from random import randint
+
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
+class IamGodError(Exception):
+	pass
+	
+class DrunkError(Exception):
+	pass
+	
+class CarCrashError(Exception):
+	pass
+
+class GluttonyError(Exception):
+	pass
+
+class DepressionError(Exception):
+	pass
+
+class SuicideError(Exception):
+	pass
+
+exceptions = [IamGodError, DrunkError, CarCrashError, GluttonyError, DepressionError, SuicideError]
+
+def one_day():
+	if randint(1, 13) == 13:
+		raise exceptions[randint(0, 5)]
+	return randint(1, 7)
+	
+carma = 0
+with open('log.txt', 'w') as file:
+	while carma < ENLIGHTENMENT_CARMA_LEVEL:
+		try:
+			carma += one_day()		
+		except IamGodError:
+			file.write(f'I am God error, my carma = {carma}\n')
+		except DrunkError:
+			file.write(f'Drunk error, my carma = {carma}\n')
+		except CarCrashError:
+			file.write(f'Car crash error, my carma = {carma}\n')
+		except GluttonyError:
+			file.write(f'Gluttony error, my carma = {carma}\n')
+		except DepressionError:
+			file.write(f'Depression error, my carma = {carma}\n')
+		except SuicideError:
+			file.write(f'Suicide error, my carma = {carma}\n')
+		except:
+			file.write(f'Неизвестная ошибка, my carma = {carma}\n')
+			
+print(carma)	
+	
 
 # https://goo.gl/JnsDqu
