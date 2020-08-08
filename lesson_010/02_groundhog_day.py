1800f8d4fe56f33eae338c3aefce6f97ff3a7405
Nullator <oDqyi1r7V3>
2020-08-08 17:01:37 +0400

    Привел в соответствие с PEP8

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -21,31 +21,40 @@ from random import randint
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
+
 class IamGodError(Exception):
 	pass
-	
+
+
 class DrunkError(Exception):
 	pass
-	
+
+
 class CarCrashError(Exception):
 	pass
+
 
 class GluttonyError(Exception):
 	pass
 
+
 class DepressionError(Exception):
 	pass
+
 
 class SuicideError(Exception):
 	pass
 
+
 exceptions = [IamGodError, DrunkError, CarCrashError, GluttonyError, DepressionError, SuicideError]
+
 
 def one_day():
 	if randint(1, 13) == 13:
 		raise exceptions[randint(0, 5)]
 	return randint(1, 7)
-	
+
+
 carma = 0
 with open('log.txt', 'w') as file:
 	while carma < ENLIGHTENMENT_CARMA_LEVEL:
@@ -63,9 +72,7 @@ with open('log.txt', 'w') as file:
 			file.write(f'Depression error, my carma = {carma}\n')
 		except SuicideError:
 			file.write(f'Suicide error, my carma = {carma}\n')
-		except:
-			file.write(f'Неизвестная ошибка, my carma = {carma}\n')
-			
+
 print(carma)	
 	
 
