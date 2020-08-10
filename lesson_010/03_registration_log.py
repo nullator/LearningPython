bac4d59cab3ff6e116b6227f32fa37b870c1b725
nullator <diver.rus@gmail.com>
2020-08-11 00:07:16 +0400

    Выполнил часть третьего учебного задания

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -21,5 +21,54 @@
 # - поле емейл НЕ содержит @ и .(точку): NotEmailError (кастомное исключение)
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
+import re
 
-# TODO здесь ваш код
+
+class NotNameError(Exception):
+	pass
+	
+	
+class NotEmailError(Exception):
+	pass
+	
+
+def check_line(line):
+	'''
+	Считывает строку, проверяет значения. При выявленииошибки вызывает исключение. В случае если ошибок нет - возвращает список со значениями
+	'''
+	name, email, age = line.split(" ")
+	if check_name(name) and check_email(email) and check_age(age):
+		return [name, email, age]
+	
+	
+def check_name(name):
+	if re.search('\d+', name) is not None:
+		raise NotNameError(f'Указано имя - {name}. Имя должно содержать только буквы без цифр')
+	else: 
+		return True
+	
+
+def check_email(email):
+	if re.search('@', email) is None or re.search('.', email) is None:
+		raise NotEmailError(f'Указана несуществующая электронная почта - {email}')
+	else:
+		return True
+		
+		
+def check_age(age):
+	if age.isdigit():
+		if int(age) < 10 or int(age) > 99:
+			raise ValueError(f'Некорректное значение возраста ({age}). Возраст должен быть в промежутке 10 - 99')
+		else:
+			return True
+	else:
+		raise ValueError(f"Указано значение возраста - {age}. Значение возраста должно содержать цифры")
+	
+
+registration_good_log = []
+registrarion_bad_log = []						
+with open('registrations.txt', 'r') as file:
+	for line in file:
+		line = line[:-1]	
+		print(check_line(line))
+		
